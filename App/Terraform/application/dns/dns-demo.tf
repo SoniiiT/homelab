50ee887cedd71299bbb8380085ac2b72dbed232c
SoniiiT <Toni.Romero@live.de>
2024-06-09 14:23:06 +0200

    modified:   App/Terraform/application/dns/dns-demo.tf

--- App/Terraform/application/dns/dns-demo.tf
+++ App/Terraform/application/dns/dns-demo.tf
@@ -11,3 +11,10 @@ resource "dns_a_record_set" "demo2" {
     addresses = [ "192.168.178.22" ]
     ttl = 3600
 }
+
+resource "dns_a_record_set" "demo3" {
+    zone = "demo.soniiit.net."
+    name = "demo3"
+    addresses = [ "192.168.178.22" ]
+    ttl = 3600
+}
