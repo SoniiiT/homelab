bcb51c029da71e16c79e073a3698ff01f65316fb
SoniiiT <Toni.Romero@live.de>
2024-06-10 23:09:29 +0200

    modified:   App/Ansible/inventory.yaml 	modified:   App/Kestra/Ansible/inventory.yaml 	modified:   Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_GlusterFS.sh

--- Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_GlusterFS.sh
+++ Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_GlusterFS.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install GlusterFS Repository
-sudo add-apt-repository ppa:gluster/glusterfs-7
-sudo apt update
-
 # Install GlusterFS
 sudo apt install glusterfs-server -y
 
@@ -11,5 +7,8 @@ sudo apt install glusterfs-server -y
 sudo systemctl start glusterd.service && sudo systemctl enable glusterd.service
 
 # How to use GlusterFS
-echo "To create a Peer Group use the command: gluster peer probe <hostname>"
-echo "To create a Volume use the command: gluster volume create <volume_name> replica <replica_count> <hostname>:/<path> <hostname2>:/<path> <hostname3>:/<path> force"
+echo "To create a Peer Group use the command: sudo gluster peer probe <hostname>"
+echo "To view the Peer Group Status use the command: sudo gluster peer status"
+echo "To create a Volume use the command: sudo gluster volume create <volume_name> replica <replica_count> <hostname>:/<path> <hostname2>:/<path> <hostname3>:/<path> force"
+echo "To start the Volume use the command: sudo gluster volume start <volume_name>"
+echo "To view the Volume Status use the command: sudo gluster volume status"
