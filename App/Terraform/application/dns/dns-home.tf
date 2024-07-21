52a27777153bb935c24f671bdce3a559db73f831
SoniiiT <SoniiiT@soniiit.net>
2024-07-21 17:41:30 +0200

    modified:   App/Terraform/application/dns/dns-home.tf 	modified:   App/Terraform/application/twingate/twingate-home.tf

--- App/Terraform/application/dns/dns-home.tf
+++ App/Terraform/application/dns/dns-home.tf
@@ -41,6 +41,13 @@ resource "dns_a_record_set" "truenas" {
     ttl = 3600
 }
 
+resource "dns_a_record_set" "win11-desk-01" {
+    zone = "home.soniiit.net."
+    name = "win11-desk-01"
+    addresses = [ "192.168.178.99" ]
+    ttl = 3600
+}
+
 # Hosts
 resource "dns_a_record_set" "pve_srv_01" {
     zone = "home.soniiit.net."
--- App/Terraform/application/twingate/twingate-home.tf
+++ App/Terraform/application/twingate/twingate-home.tf
@@ -548,3 +548,34 @@ resource "twingate_resource" "terraform_agent_01" {
 
     is_active = true
 }
+
+resource "twingate_resource" "win11-desk-01" {
+    name = "win11-desk-01"
+    address = "192.168.178.99"
+    alias = "win11-desk-01.home.soniiit.net"
+    remote_network_id = twingate_remote_network.net_home.id
+
+    security_policy_id = data.twingate_security_policy.policy_home.id
+
+    protocols = {
+        allow_icmp = true
+        tcp = {
+            policy = "RESTRICTED"
+            ports = ["22"]
+        }
+        udp = {
+            policy = "DENY_ALL"
+        }
+    }
+
+    dynamic "access_group" {
+        for_each = [twingate_group.home.id]
+        content {
+            group_id = access_group.value
+            security_policy_id = data.twingate_security_policy.policy_home.id
+            usage_based_autolock_duration_days = 30
+        }
+    }
+
+    is_active = true
+}
