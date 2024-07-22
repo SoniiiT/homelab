c6cf995a635fb07fdb4f79f5a67c11f0e2b6c5ac
SoniiiT <SoniiiT@soniiit.net>
2024-07-22 20:41:00 +0200

    new file:   App/Terraform/application/dns/dns-gameserver.tf

---
+++ App/Terraform/application/dns/dns-gameserver.tf
@@ -0,0 +1,19 @@
+# Vanilla | Minecraft | v1.21
+resource "dns_a_record_set" "mc_vanilla_121" {
+    zone = "home.soniiit.net."
+    name = "mc-121"
+    addresses = [ "192.168.178.30" ]
+    ttl = 3600
+}
+
+resource "dns_srv_record_set" "mc_121_tcp" {
+    zone = "home.soniiit.net."
+    name = "_minecraft._tcp.mc-121"
+    srv {
+    priority = 0
+    weight   = 0
+    target   = "mc-121.home.soniiit.net."
+    port     = 27000
+    }
+    ttl = 300
+}
