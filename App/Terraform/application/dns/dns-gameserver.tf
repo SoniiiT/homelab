abaa7a2e6f732e93cdd9718228138df4d9071171
SoniiiT <SoniiiT@soniiit.net>
2024-07-22 20:57:32 +0200

    modified:   App/Terraform/application/dns/dns-gameserver.tf

--- App/Terraform/application/dns/dns-gameserver.tf
+++ App/Terraform/application/dns/dns-gameserver.tf
@@ -1,18 +1,18 @@
 # Vanilla | Minecraft | v1.21
-resource "dns_a_record_set" "mc_vanilla" {
+resource "dns_a_record_set" "mcvanilla" {
     zone = "home.soniiit.net."
-    name = "mc-vanilla"
+    name = "mcvanilla"
     addresses = [ "192.168.178.30" ]
     ttl = 3600
 }
 
-resource "dns_srv_record_set" "mc_vanilla_tcp" {
+resource "dns_srv_record_set" "mcvanilla_tcp" {
     zone = "home.soniiit.net."
-    name = "_minecraft._tcp.mc-vanilla"
+    name = "_minecraft._tcp.mcvanilla"
     srv {
     priority = 0
     weight   = 0
-    target   = "mc-vanilla.home.soniiit.net."
+    target   = "mcvanilla.home.soniiit.net."
     port     = 27000
     }
     ttl = 3600
