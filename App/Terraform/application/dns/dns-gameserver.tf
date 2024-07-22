5f68d178220f739d9285052ed883eaa20396684d
SoniiiT <SoniiiT@soniiit.net>
2024-07-22 20:51:19 +0200

    modified:   App/Terraform/application/dns/dns-gameserver.tf

--- App/Terraform/application/dns/dns-gameserver.tf
+++ App/Terraform/application/dns/dns-gameserver.tf
@@ -6,13 +6,6 @@ resource "dns_a_record_set" "mc_vanilla_121" {
     ttl = 3600
 }
 
-resource "dns_a_record_set" "mc_vanilla_test" {
-    zone = "home.soniiit.net."
-    name = "mc-test"
-    addresses = [ "192.168.178.30" ]
-    ttl = 3600
-}
-
 resource "dns_srv_record_set" "mc_121_tcp" {
     zone = "home.soniiit.net."
     name = "_minecraft._tcp"
@@ -21,12 +14,6 @@ resource "dns_srv_record_set" "mc_121_tcp" {
     weight   = 0
     target   = "mc-121.home.soniiit.net."
     port     = 27000
-    }
-    srv {
-    priority = 0
-    weight   = 0
-    target   = "mc-test.home.soniiit.net."
-    port     = 27001
     }
     ttl = 300
 }
