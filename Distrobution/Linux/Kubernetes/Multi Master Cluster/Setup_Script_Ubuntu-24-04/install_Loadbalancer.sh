e37e04f6725e28d68b9d3f9620e16f4a404c8a8d
SoniiiT <SoniiiT@soniiit.net>
2024-08-10 12:02:18 +0200

    Docker und K8s installation füt LB deaktiviert

--- Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_Loadbalancer.sh
+++ Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_Loadbalancer.sh
@@ -154,14 +154,14 @@ backend kubernetes-backend
 systemctl restart haproxy && systemctl enable haproxy
 
 # Install Docker
-sudo apt install -y docker.io
+# sudo apt install -y docker.io
 
 # Install Kubernetes
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# sudo apt update
+# sudo apt install -y kubelet kubeadm kubectl
+# sudo apt-mark hold kubelet kubeadm kubectl
 
 # Deactivate swap
 sudo swapoff -a
