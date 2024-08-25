d1c526754c2fa0d6cf76032cb08ae2f7afb5d916
SoniiiT <SoniiiT@soniiit.net>
2024-08-25 14:53:02 +0200

    Updated K8s Code to newer Version

--- Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_Loadbalancer.sh
+++ Distrobution/Linux/Kubernetes/Multi Master Cluster/Setup_Script_Ubuntu-24-04/install_Loadbalancer.sh
@@ -180,8 +180,10 @@ systemctl restart haproxy && systemctl enable haproxy
 sudo apt install -y docker.io
 
 # Install Kubernetes
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
