235aa4f9c7f1eba0c1e54bf7a692d720d9944377
SoniiiT <SoniiiT@soniiit.net>
2024-08-30 01:02:49 +0200

    Deleted K8s

--- App/Terraform/application/dns/dns-dev.tf
+++ App/Terraform/application/dns/dns-dev.tf
@@ -1,66 +1,66 @@
-# K8s Cluster
-resource "dns_a_record_set" "k8s_soniiit_vip" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-vip"
-    addresses = [ "192.168.178.40" ]
-    ttl = 3600
-}
+# # K8s Cluster
+# resource "dns_a_record_set" "k8s_soniiit_vip" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-vip"
+#     addresses = [ "192.168.178.40" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_cp01" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-cp01"
-    addresses = [ "192.168.178.43" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_cp01" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-cp01"
+#     addresses = [ "192.168.178.43" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_cp02" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-cp02"
-    addresses = [ "192.168.178.44" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_cp02" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-cp02"
+#     addresses = [ "192.168.178.44" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_cp03" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-cp03"
-    addresses = [ "192.168.178.45" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_cp03" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-cp03"
+#     addresses = [ "192.168.178.45" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_lb01" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-lb01"
-    addresses = [ "192.168.178.41" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_lb01" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-lb01"
+#     addresses = [ "192.168.178.41" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_lb02" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-lb02"
-    addresses = [ "192.168.178.42" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_lb02" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-lb02"
+#     addresses = [ "192.168.178.42" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_w01" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-w01"
-    addresses = [ "192.168.178.46" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_w01" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-w01"
+#     addresses = [ "192.168.178.46" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_w02" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-w02"
-    addresses = [ "192.168.178.47" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_w02" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-w02"
+#     addresses = [ "192.168.178.47" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "k8s_soniiit_w03" {
-    zone = "dev.soniiit.net."
-    name = "k8s-soniiit-w03"
-    addresses = [ "192.168.178.48" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "k8s_soniiit_w03" {
+#     zone = "dev.soniiit.net."
+#     name = "k8s-soniiit-w03"
+#     addresses = [ "192.168.178.48" ]
+#     ttl = 3600
+# }
 
 # K3s Cluster
 resource "dns_a_record_set" "k3s_soniiit_vip" {
