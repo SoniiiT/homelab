f88ec1862e4fc2170c65bfe509f3ad7f75709c04
SoniiiT <SoniiiT@soniiit.net>
2024-09-28 16:40:41 +0200

    Add DNS A record set for CrowdSec

--- App/Terraform/application/dns/kubernetes.tf
+++ App/Terraform/application/dns/kubernetes.tf
@@ -71,3 +71,10 @@ resource "dns_a_record_set" "ceph" {
     addresses = [ "192.168.178.24" ]
     ttl = 3600
 }
+
+resource "dns_a_record_set" "crowdsec" {
+    zone = "home.soniiit.net."
+    name = "crowdsec"
+    addresses = [ "192.168.178.24" ]
+    ttl = 3600
+}
