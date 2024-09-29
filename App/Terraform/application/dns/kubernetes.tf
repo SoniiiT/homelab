50709129d28ec3ef158496678be1c69de41fd8a3
SoniiiT <SoniiiT@soniiit.net>
2024-09-29 18:49:40 +0200

    Entferne K3s-DNS-Einträge aus der Terraform-Konfiguration und kommentiere bestehende Einträge für Virtual IPs und Container-DNS.

--- App/Terraform/application/dns/kubernetes.tf
+++ App/Terraform/application/dns/kubernetes.tf
@@ -1,4 +1,4 @@
-# Virtual IP
+# # Virtual IP
 # resource "dns_a_record_set" "k3s_soniiit_vip" {
 #     zone = "home.soniiit.net."
 #     name = "k3s-soniiit-vip"
@@ -23,7 +23,7 @@
 
 # resource "dns_a_record_set" "k3s_soniiit_server03" {
 #     zone = "home.soniiit.net."
-#     name = "k3s-soniiit-server01"
+#     name = "k3s-soniiit-server03"
 #     addresses = [ "192.168.178.13" ]
 #     ttl = 3600
 # }
@@ -38,7 +38,7 @@
 
 # resource "dns_a_record_set" "k3s_soniiit_agent02" {
 #     zone = "home.soniiit.net."
-#     name = "k3s-soniiit-agent01"
+#     name = "k3s-soniiit-agent02"
 #     addresses = [ "192.168.178.15" ]
 #     ttl = 3600
 # }
@@ -50,52 +50,52 @@
 #     ttl = 3600
 # }
 
-# Containers
-resource "dns_a_record_set" "traefik" {
-    zone = "home.soniiit.net."
-    name = "traefik"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# # Containers
+# resource "dns_a_record_set" "traefik" {
+#     zone = "home.soniiit.net."
+#     name = "traefik"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "nginx" {
-    zone = "home.soniiit.net."
-    name = "nginx"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "nginx" {
+#     zone = "home.soniiit.net."
+#     name = "nginx"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "ceph" {
-    zone = "home.soniiit.net."
-    name = "ceph"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "ceph" {
+#     zone = "home.soniiit.net."
+#     name = "ceph"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "crowdsec" {
-    zone = "home.soniiit.net."
-    name = "crowdsec"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "crowdsec" {
+#     zone = "home.soniiit.net."
+#     name = "crowdsec"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "rancher" {
-    zone = "home.soniiit.net."
-    name = "rancher"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "rancher" {
+#     zone = "home.soniiit.net."
+#     name = "rancher"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "portainer" {
-    zone = "home.soniiit.net."
-    name = "portainer"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "portainer" {
+#     zone = "home.soniiit.net."
+#     name = "portainer"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
 
-resource "dns_a_record_set" "argocd" {
-    zone = "home.soniiit.net."
-    name = "argocd"
-    addresses = [ "192.168.178.24" ]
-    ttl = 3600
-}
+# resource "dns_a_record_set" "argocd" {
+#     zone = "home.soniiit.net."
+#     name = "argocd"
+#     addresses = [ "192.168.178.24" ]
+#     ttl = 3600
+# }
