19d27087fa80845403e3aeab5324595896f36c8e
SoniiiT <soniiit@soniiit.net>
2024-09-30 11:03:27 +0200

    Füge DNS-A-Record für Kestra hinzu und erstelle IngressRoute mit selbstsigniertem TLS-Zertifikat

--- App/Terraform/application/dns/kubernetes.tf
+++ App/Terraform/application/dns/kubernetes.tf
@@ -99,3 +99,10 @@ resource "dns_a_record_set" "argocd" {
     addresses = [ "192.168.178.24" ]
     ttl = 3600
 }
+
+resource "dns_a_record_set" "kestra_kube" {
+    zone = "home.soniiit.net."
+    name = "kestra-kube"
+    addresses = [ "192.168.178.24" ]
+    ttl = 3600
+}
