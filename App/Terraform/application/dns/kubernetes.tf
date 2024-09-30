5809f60cd7f30d7675019475b144dc9f86f14946
SoniiiT <soniiit@soniiit.net>
2024-09-30 11:49:05 +0200

    Füge DNS-A-Record für Homepage hinzu und erstelle IngressRoute mit selbstsigniertem TLS-Zertifikat

--- App/Terraform/application/dns/kubernetes.tf
+++ App/Terraform/application/dns/kubernetes.tf
@@ -106,3 +106,10 @@ resource "dns_a_record_set" "kestra_kube" {
     addresses = [ "192.168.178.24" ]
     ttl = 3600
 }
+
+resource "dns_a_record_set" "homepage_kube" {
+    zone = "home.soniiit.net."
+    name = "homepage-kube"
+    addresses = [ "192.168.178.24" ]
+    ttl = 3600
+}
