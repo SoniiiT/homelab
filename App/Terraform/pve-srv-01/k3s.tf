d0ead25207ac783c186d17f291f8b8c2e3df4683
SoniiiT <soniiit@soniiit.net>
2024-12-05 21:19:14 +0100

    modified:   App/Terraform/pve-srv-01/k3s.tf

--- App/Terraform/pve-srv-01/k3s.tf
+++ App/Terraform/pve-srv-01/k3s.tf
@@ -1,4 +1,4 @@
-/* resource "proxmox_vm_qemu" "k3s_soniiit_server01" {
+resource "proxmox_vm_qemu" "k3s_soniiit_server01" {
 
     # VM General Settings
     target_node = "pve-srv-01"
@@ -325,4 +325,4 @@ resource "proxmox_vm_qemu" "k3s_soniiit_agent02" {
     # (Optional) Add your Public SSH KEY
     sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCwS2E+9u7DXG01ViDPKTObWcqb3MZ1+bkHrfOl2e0FokVv8QI6v0LtJgPc5i8eEunQlK29GsLmf9s1cKLN8LdgT7VXknSg2tsNtL1/nDFrSwRnuu5HJXmxjrdpPFyBSq5h/hOwwImhT5vSRdwfNF9Y4N0kwEghzu1/7JJDF8eAW66DBrBeMgz28U4IohCiRm+YvGhsvRfqeD9ztd+Wb0xcb485yotE8LnDAzGsfSwQ4GrOaoiLYdzZAVNiqXKrZhs7YpyvTu6ngnA+sGH+w0Igms++eF9ElQkfgz4LHVUdjVaySC41YWqE2nQacVqefEPcCIKnaAwRlvtBzBSXujCCI4//py/538SWLYrcmGjtNBKkBA0KVbPP+R7qq387Hr+bjjoXtIARsuqqj6gCzmYxrGiors84vt+dYMWNzTj9tvOVkNFpsIu3NER2e1yXgjVXSpPZDKERTCcDoNIwLI91+6EpMnXYz8MtH80CIHFJNSwflc5wud2ebSEGmSfGgJ0djAN4KWIhg/J+RgcZ/o/dvew6GxRYbv9EDZ3UXSaSEeKvjGLXAPV5MhQuIB1iavzV6CO3ntTKvqYHFyxSkWTXTyllGtsAj3N2DLA8CSnswsBXmO0FleEhjwAUzCA3itDK+/oBipPFbna3x0c+qcb8GJSsYpNIvTqdFb92DgUiPQ== "
 }
- */
+
