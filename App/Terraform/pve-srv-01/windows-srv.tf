883847c95fef4205e8fb8b2afef674bd1d9cde8a
SoniiiT <soniiit@soniiit.net>
2024-12-19 23:01:14 +0100

    modified:   App/Terraform/pve-srv-01/windows-srv.tf

--- App/Terraform/pve-srv-01/windows-srv.tf
+++ App/Terraform/pve-srv-01/windows-srv.tf
@@ -129,3 +129,69 @@ resource "proxmox_vm_qemu" "dc_srv_02" {
     # Skip IPv6 Validation
     skip_ipv6 = true
 }
+
+resource "proxmox_vm_qemu" "win_srv_01" {
+
+    # VM General Settings
+    target_node = "pve-srv-01"
+    vmid = "0"
+    name = "win-srv-01"
+    tags = "windows"
+    desc = "Veeam Backup & Replication Server"
+    automatic_reboot = false
+
+    # The destination resource pool for the new VM
+    pool = "Windows"
+
+    # VM Advanced General Settings
+    onboot = true
+    boot = "order=scsi0"
+    bootdisk = "virtio"
+    scsihw = "virtio-scsi-pci"
+
+    # VM OS Settings
+    full_clone = true
+    clone = "windows-server-2k22"
+    bios = "ovmf"
+
+    # VM System Settings
+    agent = 1
+
+    # VM CPU Settings
+    cores = 2
+    sockets = 1
+    cpu = "kvm64"
+
+    # VM Memory Settings
+    memory = 4096
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    # Efi Disk Settings
+    efidisk {
+        efitype = "4m"
+        storage = "M2-Storage"
+    }
+
+    # VM Disk Settings Cloud-Init
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    size               = 64
+                    storage            = "M2-Storage"
+                }
+            }
+        }
+    }
+
+    # IP Address and Gateway
+    ipconfig0 = "ip=dhcp,ip6=dhcp"
+
+    # Skip IPv6 Validation
+    skip_ipv6 = true
+}
